031d5b3bf5e620d934371bf47dab31049fb85d91
fedotovps <fedotovps@bk.ru>
2024-11-16 16:08:05 +0300

    Модалка оплаты (дополнения)

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["51.250.107.183"], // Указываем только IP-адрес без порта
+    domains: ["130.193.53.157"], // Указываем только IP-адрес без порта
   },
 };
 
--- src/api/server/configApi.ts
+++ src/api/server/configApi.ts
@@ -1,3 +1,3 @@
-export const host = "http://51.250.107.183:";
+export const host = "http://130.193.53.157:";
 export const port = "3000";
 export const baseUrl = `${host}${port}/api/`;
