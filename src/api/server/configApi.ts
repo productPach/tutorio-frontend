e0ca9c105f75fa7b57739d47add263bedd142f26
fedotovps <fedotovps@bk.ru>
2025-07-27 18:18:35 +0300

    фикс

--- src/api/server/configApi.ts
+++ src/api/server/configApi.ts
@@ -1,33 +1,32 @@
 import "dotenv/config";
 
 // Для окружения Docker-compose
-// //export const host = `https://51.250.20.10:`;
-// export const host = `https://dev-tutorio.ru:`;
-// export const port = `3000`;
-// export const getBackendUrl = () => {
-//   if (typeof window !== "undefined") {
-//     // Клиент
-//     //return process.env.NEXT_PUBLIC_CDN_URL || "http://84.201.140.78:3000";
-//     //return process.env.NEXT_PUBLIC_CDN_URL || "https://51.250.20.10";
-//     return process.env.NEXT_PUBLIC_CDN_URL || "https://dev-tutorio.ru";
-//   } else {
-//     // Сервер (Next.js внутри докера)
-//     return `http://${process.env.NEXT_PUBLIC_BACKEND_HOST || "tutorio-express"}:${process.env.NEXT_PUBLIC_BACKEND_PORT || "3000"}`;
-//   }
-// };
-//export const baseUrl = `${host}/api/`;
+//export const host = `https://51.250.20.10:`;
+export const host = `https://dev-tutorio.ru:`;
+export const port = `3000`;
+export const getBackendUrl = () => {
+  if (typeof window !== "undefined") {
+    // Клиент
+    //return process.env.NEXT_PUBLIC_CDN_URL || "http://84.201.140.78:3000";
+    //return process.env.NEXT_PUBLIC_CDN_URL || "https://51.250.20.10";
+    return process.env.NEXT_PUBLIC_CDN_URL || "https://dev-tutorio.ru";
+  } else {
+    // Сервер (Next.js внутри докера)
+    return `http://${process.env.NEXT_PUBLIC_BACKEND_HOST || "tutorio-express"}:${process.env.NEXT_PUBLIC_BACKEND_PORT || "3000"}`;
+  }
+};
+export const baseUrl = `${host}/api/`;
 // ------------------------------------------------------------------------
 // ------------------------------------------------------------------------
 // ------------------------------------------------------------------------
 // ------------------------------------------------------------------------
 
 // Для окружения фронт локально, сервер удаленно
-export const host = `http://${process.env.NEXT_PUBLIC_IP}:`;
-export const port = process.env.NEXT_PUBLIC_PORT;
-export const getBackendUrl = () => {
-    // Клиент (браузер)
-    return `${host}${port}` || "http://localhost:3000";
-};
+// export const host = `http://${process.env.NEXT_PUBLIC_IP}:`;
+// export const port = process.env.NEXT_PUBLIC_PORT;
+// export const getBackendUrl = () => {
+//     // Клиент (браузер)
+//     return `${host}${port}` || "http://localhost:3000";
+// };
 
-export const baseUrl = `${host}${port}/api/`;
-
+//export const baseUrl = `${host}${port}/api/`;
